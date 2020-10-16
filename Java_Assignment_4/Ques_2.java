045e82300e8c339880c294dfdcf3a3933c1bba60
pulkitgupta870 <69986705+pulkitgupta870@users.noreply.github.com>
2020-10-16 21:14:03 +0530

    Add files via upload

---
+++ Java_Assignment_4/Ques_2.java
@@ -0,0 +1,9 @@
+public class Ques_2 {
+	public static void main(String[] args) {
+		int first_var = -1;
+		byte second_var = (byte)first_var;
+		char third_var = (char)second_var;
+		int fourth_var = (int)third_var;
+		System.out.println(fourth_var);
+	}
+}
